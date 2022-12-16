01b2f1fa6fcef5bf932441ba1cbfbd89f8c8e339
Joshua Hiller <joshua.hiller@crowdstrike.com>
2022-12-16 02:39:54 -0500

    Linting

--- ocsf/__init__.py
+++ ocsf/__init__.py
@@ -1,4 +1,4 @@
 """OCSF file conversion, upload."""
 from .ocsf import transform_fdr_data_to_ocsf_data, upload_parquet_files_to_s3
 
-ALL = ["transform_fdr_data_to_ocsf_data", "upload_parquet_files_to_s3"]
+__all__ = ["transform_fdr_data_to_ocsf_data", "upload_parquet_files_to_s3"]
